633e402121143f51d366d9b82023d6769858e980
Chingiz Olzhabaev <olzhabaev.sh@outlook.com>
2024-12-19 19:05:49 +0100

    add dashboard

--- src/apps/Chatier.Apps.SignalrService/Program.cs
+++ src/apps/Chatier.Apps.SignalrService/Program.cs
@@ -41,6 +41,12 @@ builder.Host.UseOrleans(static siloBuilder =>
     siloBuilder.AddMemoryStreams("MemoryStreamProvider");
     // configure reminder service
     siloBuilder.UseInMemoryReminderService();
+
+    // configure dashboard
+    siloBuilder.UseDashboard(x => 
+    {
+        x.HostSelf = true;
+    });
 });
 
 builder.Services.AddCors(options =>
@@ -77,58 +83,6 @@ app.UseHttpsRedirection();
 
 app.MapHub<UserHub>("/userHub");
 
-var apiGroup = app.MapGroup("/api");
-
-apiGroup.MapGet("/createUsers", async([FromServices] IClusterClient clusterClient) => 
-{
-    var alphaGrain = clusterClient.GetGrain<IUserGrain>("alpha");
-    var bravoGrain = clusterClient.GetGrain<IUserGrain>("bravo");
-
-    var alphChatGrain = clusterClient.GetGrain<IUserChatGrain>("alpha");
-    var chatName = await alphChatGrain.CreateChatAsync("bravo");
-
-    var chatGrain = clusterClient.GetGrain<IChatGrain>(chatName);
-
-    var msg1 = await chatGrain.SendMessageAsync("alpha", "Hello, bravo!");
-    var msg2 = await chatGrain.SendMessageAsync("bravo", "Hello, alpha!");
-
-    var msg3 = await chatGrain.SendMessageAsync("alpha", "How are you?");
-    var msg4 = await chatGrain.SendMessageAsync("bravo", "I'm fine, thank you!");
-
-    var msg5 = await chatGrain.SendMessageAsync("alpha", "Good to hear that!");
-
-    var messageIds = new[] { msg1, msg2, msg3, msg4, msg5 };
-
-    var alphaMessageGrain = clusterClient.GetGrain<IUserMessageNotificationGrain>("alpha");
-    var bravoMessageGrain = clusterClient.GetGrain<IUserMessageNotificationGrain>("bravo");
-
-    foreach (var item in messageIds)
-    {
-        var alphaNotificationId = await alphaMessageGrain.GetNotificationIdAsync(item);
-        var bravoNotificationId = await bravoMessageGrain.GetNotificationIdAsync(item);
-
-        await alphaGrain.ConfirmNotificationAsync(alphaNotificationId);
-        await bravoGrain.ConfirmNotificationAsync(bravoNotificationId);
-    }
-
-    return Results.Ok(chatName);
-});
-
-apiGroup.MapGet(
-    "/send/{fromUser}/to/{toUser}/{message}",
-    async (
-        [FromQuery] string fromUser,
-        [FromQuery] string toUser,
-        [FromQuery] string message,
-        [FromServices] IClusterClient clusterClient) => 
-    {
-        var names = new[] { fromUser, toUser };
-        var chatName = string.Join("-", names.OrderBy(n => n));
-        var chatGrain = clusterClient.GetGrain<IChatGrain>(chatName);
-
-        await chatGrain.SendMessageAsync(fromUser, message);
-
-        return Results.Ok();
-    });
+app.Map("/dashboard", x => x.UseOrleansDashboard());
 
 app.Run();
